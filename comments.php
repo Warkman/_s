aeed949b2910ba40e0b1d2a40a5d9e1eadb3aa9f
Bjarni Wark <bjarni@bjarniwark.com>
2015-06-08 14:31:39 +1000

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying comments.
+ * The template for displaying comments. I made this CHANGE 8th June 2015
  *
  * The area of the page that contains both current comments
  * and the comment form.
